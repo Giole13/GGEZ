639c7137749f22e3de2a8843d8d35baa88f4eaba
Giole13 <gudwns4f12@gmail.com>
2022-12-28 17:12:31 +0900

    [HJ] 오늘은 클래스를 배웠다

---
+++ 2022.12/test11/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test11
+{
+    internal class Class1
+    {
+        public static void Main()
+        {
+            string allMoney = Console.ReadLine();
+            string[] word = allMoney.Split(" ");
+
+            int num1 = (int)word[0];
+
+        }
+    }
+}
